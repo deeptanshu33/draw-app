f022eb7474786263123afcde4eb8e3703a0da0b7
Deeptanshu Prasad <Deeptanshu.me@outlook.com>
2025-12-04 14:49:44 +0530

    FIX prismClient error

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -3,20 +3,40 @@ import jwt from 'jsonwebtoken'
 import { JWT_SECRET } from '@repo/backend-common/config'
 import { authMiddleware } from './middleware'
 import { CreateRoomSchema, SignInSchema, SignupSchema } from '@repo/common/types'
+import { prismaClient } from "@repo/db/client"
 
 const app = express()
+app.use(express.json())
 
-app.post('/signup', (req, res) => {
+app.post('/signup', async (req, res) => {
     //get username password
-    const data = SignupSchema.safeParse(req.body)
-    if(!data.success){
+    const parsedData = SignupSchema.safeParse(req.body)
+    if (!parsedData.success) {
         res.json({
             message: "Incorrect sign up inputs"
         })
         return;
     }
-    const username = req.body.get('username')
-    const password = req.body.get('password')
+
+    try {
+        const user = await prismaClient.user.create({
+            data: {
+                email: parsedData.data.username,
+                password: parsedData.data.password,
+                name: parsedData.data.name
+            }
+        })
+
+        res.json({
+            userId: user.id
+        })
+    }
+    catch(e) {
+        console.log(e)
+        res.status(411).json({
+            message: "User with provided email already exists"
+        })
+    }
 
 
 })
@@ -26,7 +46,7 @@ app.post('/signin', (req, res) => {
     const password = req.body.get('password')
 
     const data = SignInSchema.safeParse(req.body)
-    if(!data.success){
+    if (!data.success) {
         res.json({
             message: "Incorrect sign in inputs"
         })
@@ -45,7 +65,7 @@ app.post('/signin', (req, res) => {
 
 app.post('/room', authMiddleware, (req, res) => {
     const data = CreateRoomSchema.safeParse(req.body)
-    if(!data.success){
+    if (!data.success) {
         res.json({
             message: "Incorrect room inputs"
         })
--- packages/db/prisma/migrations/20251204062422_init_schema/migration.sql
+++ packages/db/prisma/migrations/20251204062422_init_schema/migration.sql
@@ -3,8 +3,8 @@ CREATE TABLE "User" (
     "id" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
+    "photo" TEXT,
     "name" TEXT NOT NULL,
-    "photo" TEXT NOT NULL,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
@@ -13,21 +13,24 @@ CREATE TABLE "User" (
 CREATE TABLE "Room" (
     "id" SERIAL NOT NULL,
     "slug" TEXT NOT NULL,
-    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "adminId" TEXT NOT NULL,
 
     CONSTRAINT "Room_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "ChatHistory" (
+CREATE TABLE "Chat" (
     "id" SERIAL NOT NULL,
-    "message" TEXT NOT NULL,
     "roomId" INTEGER NOT NULL,
+    "message" TEXT NOT NULL,
     "userId" TEXT NOT NULL,
 
-    CONSTRAINT "ChatHistory_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Chat_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Room_slug_key" ON "Room"("slug");
@@ -36,7 +39,7 @@ CREATE UNIQUE INDEX "Room_slug_key" ON "Room"("slug");
 ALTER TABLE "Room" ADD CONSTRAINT "Room_adminId_fkey" FOREIGN KEY ("adminId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "ChatHistory" ADD CONSTRAINT "ChatHistory_roomId_fkey" FOREIGN KEY ("roomId") REFERENCES "Room"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Chat" ADD CONSTRAINT "Chat_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "ChatHistory" ADD CONSTRAINT "ChatHistory_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Chat" ADD CONSTRAINT "Chat_roomId_fkey" FOREIGN KEY ("roomId") REFERENCES "Room"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
