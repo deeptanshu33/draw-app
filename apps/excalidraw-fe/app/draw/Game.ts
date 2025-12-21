e8507dfaed682505fecb0506075929747f47531e
Deeptanshu Prasad <Deeptanshu.me@outlook.com>
2025-12-21 19:32:05 +0530

    feat: add pan support - panning changes the area of the world coordinates currently in the viewport - shapes can be drawn and saved after panning - shapes are drawn and saved in world coordinates - no changes in drawing existing shapes, they are converted to viewport coordinates using setTransform

--- apps/excalidraw-fe/app/draw/Game.ts
+++ apps/excalidraw-fe/app/draw/Game.ts
@@ -1,10 +1,17 @@
 import { start } from "repl";
 import { toolOptions } from "../components/Canvas";
 import { getExistingShapes } from "./http";
+import { screenToWorldCoordinates } from "./utils"
 
 type Point = {
     x: number,
     y: number
+}
+
+type viewportObject = {
+    x: number,
+    y: number,
+    scale: number
 }
 
 type Shape = {
@@ -32,15 +39,22 @@ export class Game {
     private currTool: toolOptions
     private currPath: Point[]
     private clicked: boolean
-    private viewportTransform = {}
+    private viewportTransform:viewportObject
     private startX = 0
     private startY = 0
+    private viewportX = 0
+    private viewportY = 0
 
     constructor(canvas: HTMLCanvasElement, roomId: string, socket: WebSocket) {
         this.canvas = canvas
         this.ctx = canvas.getContext("2d")!
         this.existingShapes = []
         this.currPath = []
+        this.viewportTransform = {
+            x: 0,
+            y: 0,
+            scale: 1
+        }
         this.roomId = roomId
         this.clicked = false
         this.currTool = "circle"
@@ -52,11 +66,6 @@ export class Game {
 
 
     async init() {
-        this.viewportTransform = {
-            x: 0,
-            y: 0,
-            scale: 1
-        }
         this.existingShapes = await getExistingShapes(this.roomId)
         this.clearCanvas()
     }
@@ -64,14 +73,39 @@ export class Game {
     destroy() {
         this.canvas?.removeEventListener("mousedown", this.mousedownHandler)
         this.canvas?.removeEventListener("mouseup", this.mouseupHandler)
+        this.canvas.removeEventListener("mouseleave", this.mouseupHandler)
+        window.removeEventListener("mouseup", this.mouseupHandler)
         this.canvas?.removeEventListener("mousemove", this.mousemoveHandler)
     }
 
+    updatePanning = (e: MouseEvent) => {
+        const localX = e.clientX
+        const localY = e.clientY
+
+        this.viewportTransform.x += localX - this.viewportX
+        this.viewportTransform.y += localY - this.viewportY
+
+        this.viewportX = localX
+        this.viewportY = localY
+    }
+
     clearCanvas() {
+        this.ctx.setTransform(1, 0, 0, 1, 0, 0)
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
-        this.ctx.fillStyle = "rgba(0, 0, 0)"
+
+        this.ctx.fillStyle = "black"
         this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height)
+
+        this.ctx.setTransform(
+            this.viewportTransform.scale,
+            0,
+            0,
+            this.viewportTransform.scale,
+            this.viewportTransform.x,
+            this.viewportTransform.y    
+        )
         this.ctx.strokeStyle = "rgba(255, 255, 255)"
+
         this.existingShapes.map((shape) => {
             if (shape.type === "rect") {
                 this.ctx.strokeRect(shape.x, shape.y, shape.width, shape.height)
@@ -82,10 +116,12 @@ export class Game {
                 this.ctx.stroke()
             }
             if (shape.type === "pencil"){
+                if(shape.points.length<2) return;
                 const [firstPoint, ...rest] = shape.points
                 this.ctx.lineCap = "round"
                 this.ctx.beginPath()
                 this.ctx.moveTo(firstPoint.x, firstPoint.y)
+                console.log(firstPoint)
                 rest.forEach(p => this.ctx.lineTo(p.x, p.y))
                 this.ctx.stroke()
             }
@@ -93,6 +129,9 @@ export class Game {
     }
 
     setTool(tool: toolOptions) {
+        this.clicked = false
+        this.currPath = []
+
         this.currTool = tool
     }
 
@@ -116,29 +155,35 @@ export class Game {
 
     mousedownHandler = (e: MouseEvent) => {
         console.log("mouseDown")
+        const {worldX: fX, worldY: fY} = screenToWorldCoordinates(e.clientX, e.clientY, this.viewportTransform.x, this.viewportTransform.y ,this.viewportTransform.scale)
         if(this.currTool=="pencil"){
             this.currPath.push({
-                x: e.clientX,
-                y: e.clientY
+                x: fX,
+                y: fY
             })
         }
         this.clicked = true;
         this.startX = e.clientX;
         this.startY = e.clientY;
+        this.viewportX = e.clientX
+        this.viewportY = e.clientY
         console.log(e.clientX);
         console.log(e.clientY);
     }
 
     mouseupHandler = (e: MouseEvent) => {
+        if(!this.clicked) return;
         this.clicked = false;
-        const width = e.clientX - this.startX;
-        const height = e.clientY - this.startY;
+        const {worldX: cwX, worldY: cwY} = screenToWorldCoordinates(e.clientX, e.clientY, this.viewportTransform.x, this.viewportTransform.y, this.viewportTransform.scale)
+        const {worldX: swX, worldY: swY} = screenToWorldCoordinates(this.startX, this.startY, this.viewportTransform.x, this.viewportTransform.y, this.viewportTransform.scale)
+        const width = cwX - swX
+        const height = cwY - swY 
         let shape: Shape | null = null;
         if (this.currTool == "rectangle") {
             shape = {
                 type: "rect",
-                x: this.startX,
-                y: this.startY,
+                x: swX,
+                y: swY,
                 width,
                 height
             }
@@ -147,12 +192,13 @@ export class Game {
             const radius = Math.abs(Math.max(width, height))
             shape = {
                 type: "circle",
-                centerX: this.startX + radius,
-                centerY: this.startY + radius,
+                centerX: swX + radius,
+                centerY: swY + radius, 
                 radius: radius
             }
         }
         else if (this.currTool == "pencil"){
+            if(this.currPath.length < 2) return;
             shape = {
                 type: "pencil",
                 points: this.currPath
@@ -174,18 +220,28 @@ export class Game {
     }
 
     mousemoveHandler = (e: MouseEvent) => {
-        if (this.clicked) {
-            const width = e.clientX - this.startX;
-            const height = e.clientY - this.startY;
+        if (this.clicked==true) {
+            const {worldX: cwX, worldY: cwY} = screenToWorldCoordinates(e.clientX, e.clientY, this.viewportTransform.x, this.viewportTransform.y, this.viewportTransform.scale)
+            const {worldX: swX, worldY: swY} = screenToWorldCoordinates(this.startX, this.startY, this.viewportTransform.x, this.viewportTransform.y, this.viewportTransform.scale)
+            
+            const width = cwX - swX
+            const height = cwY - swY
             if (this.currTool == "pencil"){
                 const currX = e.clientX
                 const currY = e.clientY
+
+                const {worldX: prevWX, worldY: prevWY} = screenToWorldCoordinates(this.startX, this.startY, this.viewportTransform.x, this.viewportTransform.y, this.viewportTransform.scale)
+                const {worldX, worldY} = screenToWorldCoordinates(currX, currY, this.viewportTransform.x, this.viewportTransform.y, this.viewportTransform.scale)
+                
                 this.ctx.lineCap = 'round'
                 this.ctx.beginPath()
-                this.ctx.moveTo(this.startX, this.startY)
-                this.ctx.lineTo(currX, currY)
+                // this.ctx.moveTo(this.startX, this.startY)
+                // this.ctx.lineTo(currX, currY)
+                this.ctx.moveTo(prevWX, prevWY)
+                this.ctx.lineTo(worldX, worldY)
                 this.ctx.stroke()
-                this.currPath.push({x:currX, y:currY})
+                
+                this.currPath.push({x:worldX, y:worldY})
                 this.startX = currX
                 this.startY = currY
                 
@@ -196,20 +252,25 @@ export class Game {
 
             if (this.currTool == "rectangle") {
                 this.ctx.strokeStyle = "rgba(255, 255, 255)"
-                this.ctx.strokeRect(this.startX, this.startY, width, height)
+                this.ctx.strokeRect(swX, swY, width, height)
             }
             else if (this.currTool == "circle") {
                 this.ctx.beginPath()
                 const radius = Math.abs(Math.max(width, height))
-                this.ctx.arc(this.startX + radius, this.startY + radius, radius, 0, 2 * Math.PI)
+                this.ctx.arc(swX + radius, swY + radius, radius, 0, 2 * Math.PI)
                 this.ctx.stroke()
             }
-            
+            else if(this.currTool == "pointer"){
+                this.updatePanning(e)
+                return;
+            }
         }
     }
 
     initMouseHandlers() {
         this.canvas?.addEventListener("mousedown", this.mousedownHandler)
+        this.canvas.addEventListener("mouseleave", this.mouseupHandler)
+        window.addEventListener("mouseup", this.mouseupHandler)
         this.canvas?.addEventListener("mouseup", this.mouseupHandler)
         this.canvas?.addEventListener("mousemove", this.mousemoveHandler)
     }
